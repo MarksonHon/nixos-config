9759ddc066487c69c7aefc9118779b861b027160
Markson Hon <markson@local.local>
2024-03-31 12:11:16 +0800

    init

---
+++ configuration.nix
@@ -0,0 +1,162 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix # Include the results of the hardware scan.
+    ./flatpak-hook.nix # Include the flatpak hook.
+    ./fonts.nix # Include the fonts configuration.
+    <home-manager/nixos>
+  ];
+
+  # Bootloader.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelParams = [ "" ];
+    extraModprobeConfig = ''
+      options snd_hda_intel power_save=0
+    '';
+  };
+
+  # Networking.
+  networking = {
+    hostName = "nixos-on-asus"; # Define your hostname.
+    networkmanager.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "zh_CN.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
+
+  # Desktop.
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+    gnome.gnome-settings-daemon.enable = true;
+  };
+
+  # Enable DConf and GNOME terminal.
+  programs = {
+    dconf.enable = true;
+    gnome-terminal.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Flatpak.
+  services.flatpak.enable = true;
+
+  #XDG Portal
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    #   extraPortals = [
+    #     pkgs.xdg-desktop-portal-gtk
+    #   ];
+  };
+
+  # Fcitx5.
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-chinese-addons fcitx5-configtool ];
+    fcitx5.waylandFrontend = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.markson = {
+    isNormalUser = true;
+    description = "Markson Hon";
+    extraGroups = [ "networkmanager" "wheel" "vboxusers" ];
+    packages = with pkgs; [ chromium ];
+  };
+  home-manager.users.markson = { pkgs, ... }: { home.stateVersion = "24.05"; };
+
+  # VirtualBox.
+  virtualisation.virtualbox.host.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    nano
+    nvtopPackages.full
+    gst_all_1.gstreamer
+    gst_all_1.gst-vaapi
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-plugins-good
+    curl
+    yaru-theme
+    intel-media-driver
+    firefox
+    fastfetch
+    p7zip
+    unzip
+    unrar
+    gnome.gnome-tweaks
+    mpv
+    vscode
+    git
+    mission-center
+    bind
+    file
+    bat
+    powershell
+  ];
+
+  # Environments.
+  environment.variables = {
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_IM_MODULE = "fcitx";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
