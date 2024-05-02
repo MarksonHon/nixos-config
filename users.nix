78ba44bf2d34e626c13111ed4ea904e366172047
Markson Hon <markson@nixos.local>
2024-05-02 18:57:39 +0800

    better zsh config 3

--- users.nix
+++ users.nix
@@ -50,18 +50,18 @@
             p10k configure
           fi
           typeset -g -A key
-          key[Home]="\$\{terminfo[khome]\}"
-          key[End]="\$\{terminfo[kend]\}"
-          key[Insert]="\$\{terminfo[kich1]\}"
-          key[Backspace]="\$\{terminfo[kbs]\}"
-          key[Delete]="\$\{terminfo[kdch1]\}"
-          key[Up]="\$\{terminfo[kcuu1]\}"
-          key[Down]="\$\{terminfo[kcud1]\}"
-          key[Left]="\$\{terminfo[kcub1]\}"
-          key[Right]="\$\{terminfo[kcuf1]\}"
-          key[PageUp]="\$\{terminfo[kpp]\}"
-          key[PageDown]="\$\{terminfo[knp]\}"
-          key[Shift-Tab]="\$\{terminfo[kcbt]\}"
+          key[Home]=''"${terminfo[khome]}"''
+          key[End]=''"${terminfo[kend]}"''
+          key[Insert]=''"${terminfo[kich1]}"''
+          key[Backspace]=''"${terminfo[kbs]}"''
+          key[Delete]=''"${terminfo[kdch1]}"''
+          key[Up]=''"${terminfo[kcuu1]}"''
+          key[Down]=''"${terminfo[kcud1]}"''
+          key[Left]=''"${terminfo[kcub1]}"''
+          key[Right]=''"${terminfo[kcuf1]}"''
+          key[PageUp]=''"${terminfo[kpp]}"''
+          key[PageDown]=''"${terminfo[knp]}"''
+          key[Shift-Tab]=''"${terminfo[kcbt]}"''
         '';
         completionInit = ''
           autoload -Uz compinit
