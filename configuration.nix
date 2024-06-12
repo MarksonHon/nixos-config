b9d47aa733c1b46539373f72cc8f6291c1a75405
Markson Hon <Markson@markson.local>
2024-06-12 21:44:10 +0800

    Update to NixOS 24.11

--- configuration.nix
+++ configuration.nix
@@ -201,11 +201,11 @@
     xorg.xrdb
     vulkan-tools
     nftables
-    kdePackages.powerdevil
     podman-tui
     glxinfo
     clinfo
     wayland-utils
+    dae
   ];
 
   # Environments.
@@ -228,6 +228,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
